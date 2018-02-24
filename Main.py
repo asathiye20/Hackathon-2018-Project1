005c2859e4bf6ce382fabefec054f5de46d6ae52
jgraybea20 <36793981+jgraybea20@users.noreply.github.com>
2018-02-24 17:18:57 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,8 +1,12 @@
-import secret   # this has our API Keys
 import tweepy
 
-auth = tweepy.OAuthHandler(secret.TWITTER_CLIENT, secret.TWITTER_SECRET)
-auth.set_access_token(secret.TWITTER_ACCESS, secret.TWITTER_ACCESS_SECRET)
+TWITTER_CLIENT = "kwykBA0bYzMqcHoFl29zK1OQa"
+TWITTER_SECRET = "xyR4hegGMoQxCiDPrqmr4CLGm41DqG1rQDDDMrQRLJwRXBYd3O"
+TWITTER_ACCESS = "967433840183709696-DRyKObUSKI7WbOmoePl0O3GBff8DYxV"
+TWITTER_ACCESS_SECRET = "Veyq0L0SKkByptUD1CYlR2XkgjYDhJObzwbigbFrTe76A"
+
+auth = tweepy.OAuthHandler(TWITTER_CLIENT, TWITTER_SECRET)
+auth.set_access_token(TWITTER_ACCESS, TWITTER_ACCESS_SECRET)
 
 api = tweepy.API(auth)
 
