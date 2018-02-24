54386703e71a9fdd447ef466ee0aa33ca6abfcd7
asathiye20 <asathiye20@maldenps.org>
2018-02-24 17:33:44 -0500

    Create Main2.py

---
+++ Main2.py
@@ -0,0 +1,14 @@
+import tweepy
+
+TWITTER_CLIENT = "kwykBA0bYzMqcHoFl29zK1OQa"
+TWITTER_SECRET = "xyR4hegGMoQxCiDPrqmr4CLGm41DqG1rQDDDMrQRLJwRXBYd3O"
+TWITTER_ACCESS = "967433840183709696-DRyKObUSKI7WbOmoePl0O3GBff8DYxV"
+TWITTER_ACCESS_SECRET = "Veyq0L0SKkByptUD1CYlR2XkgjYDhJObzwbigbFrTe76A"
+
+auth = tweepy.OAuthHandler(TWITTER_CLIENT, TWITTER_SECRET)
+
+auth.set_access_token(TWITTER_ACCESS, TWITTER_ACCESS_SECRET)
+
+api = tweepy.API(auth)
+
+api.update_status("Hello, world!!!!")
